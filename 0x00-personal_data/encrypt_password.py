554c60cbcc367e077af1eb0f78476e9acbeecb30
daveyhmariam <daveyhmariam@gmail.com>
2024-08-02 01:06:26 +0300

    check hashed password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -8,3 +8,16 @@ def hash_password(password: str) -> bytes:
     """Password hashing.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Checking if a hashed password
+
+    Args:
+        hashed_password
+        password (str):
+
+    Returns:
+        bool:
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
