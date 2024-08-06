cc7466ae65a559b618632299a167e1a4cef54c00
daveyhmariam <daveyhmariam@gmail.com>
2024-08-07 01:13:39 +0300

    template class

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+Auth class definition
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+class Auth:
+    """
+    Authentication class
+    """
+
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+
+        Args:
+            path (str):
+            excluded_paths (List[str]):
+
+        Returns:
+            bool:
+        """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """
+
+        Args:
+            request (_type_, optional): Defaults to None.
+
+        Returns:
+            str:
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+
+        Returns:
+            User:
+        """
+        return None
