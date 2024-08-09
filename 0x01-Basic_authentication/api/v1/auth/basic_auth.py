8c907a2b2f04677881cf7283a3f62ea3007eb3fc
daveyhmariam <daveyhmariam@gmail.com>
2024-08-09 23:30:28 +0300

    instance method that returns that returns the decoded value of a Base64 string

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,8 @@ Authenticating with basic auth class definition
 
 from api.v1.auth.auth import Auth
 import re
+import base64
+import binascii
 
 
 class BasicAuth(Auth):
@@ -31,3 +33,19 @@ class BasicAuth(Auth):
         match = re.match(r'^Basic\s', authorization_header)
         if match is not None:
             return authorization_header.strip(match.group())
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """returns the decoded value of a Base64 string
+
+        Args:
+            base64_authorization_header (str): encoded base64 string
+        Returns:
+            str: decoded string in utf-8 format 
+        """
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded = base64.b64decode(base64_authorization_header, validate=True)
+            return decoded.decode('utf-8')
+        except (binascii.Error, UnicodeDecodeError):
+            return None
