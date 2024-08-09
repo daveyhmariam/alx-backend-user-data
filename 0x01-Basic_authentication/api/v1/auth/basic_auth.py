252651c57201ebe36b5a7fdad008a0a2344e0b28
daveyhmariam <daveyhmariam@gmail.com>
2024-08-09 23:09:45 +0300

    instance method that returns the Base64 part of the Authorization header for a Basic Authentication:

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,6 +4,7 @@ Authenticating with basic auth class definition
 """
 
 from api.v1.auth.auth import Auth
+import re
 
 
 class BasicAuth(Auth):
@@ -11,4 +12,22 @@ class BasicAuth(Auth):
 
        Auth (class): base class for authentication
     """
-    pass
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Decode authorization header
+
+        Args:
+            authorization_header (str): request header under Authorization
+
+        Returns:
+            str: credential string
+        """
+        if (authorization_header is None
+            or
+                not isinstance(authorization_header, str)):
+            return None
+
+        match = re.match(r'^Basic\s', authorization_header)
+        if match is not None:
+            return authorization_header.strip(match.group())
