059bf2c230729a04a2605572322e8dc2037d8364
daveyhmariam <daveyhmariam@gmail.com>
2024-08-10 01:24:40 +0300

    basic

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,6 +5,7 @@ Auth class definition
 
 from flask import request
 from typing import List, TypeVar
+import re
 
 
 class Auth:
@@ -13,18 +14,19 @@ class Auth:
     """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a path requires authentication.
         """
-        Args:
-            path (str):
-            excluded_paths (List[str]):
-
-        Returns:
-            bool:
-        """
-        if path is None or excluded_paths is None or excluded_paths == []:
-            return True
-        if path in excluded_paths or (path + "/") in excluded_paths:
-            return False
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
         return True
 
     def authorization_header(self, request=None) -> str:
