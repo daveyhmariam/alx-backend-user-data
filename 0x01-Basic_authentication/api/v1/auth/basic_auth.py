9586bedf1984acf463228f496da6bc452d5f639f
daveyhmariam <daveyhmariam@gmail.com>
2024-08-09 22:27:20 +0300

    basic authentication class template

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Authenticating with basic auth class definition
+"""
+
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """class definition of basic auth
+
+       Auth (class): base class for authentication
+    """
+    pass
