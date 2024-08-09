fd8730f0b56d0c6096017553034645fba64203ff
daveyhmariam <daveyhmariam@gmail.com>
2024-08-09 22:30:44 +0300

    basic authentication class template

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,9 +14,9 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth_type = os.getenv("AUTH_TYPE")
-if auth_type:
-    from api.v1.auth.auth import Auth
-    auth = Auth()
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
 
 
 @app.errorhandler(404)
