a9f1c456f8f7eb46abe21d581f64f83bcde08a70
daveyhmariam <daveyhmariam@gmail.com>
2024-08-16 00:54:30 +0300

    user login POST method

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -38,5 +38,23 @@ def create_user():
         {"email": F"{email}", "message": "user created"}), 200)
 
 
+@app.route('/sessions',  methods=['POST'], strict_slashes=False)
+def login_sessions():
+    """handles user login authentication
+    and creates a session cookie upon successful login.
+
+    Returns:
+        _type_:
+    """
+    email = request.form.get('email', None)
+    password = request.form.get('password', None)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    resp = make_response(jsonify({"email": F"{email}",
+                                  "message": "logged in"}), 200)
+    resp.set_cookie('session_id', AUTH.create_session(email))
+    return resp
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
